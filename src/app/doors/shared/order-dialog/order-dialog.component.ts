d9907c7a26f7d83912145c122f312c374ba3b09c
Ivan <ivan.kirnev@gmail.com>
2019-04-04 02:00:33 +0300

    sent notification

--- src/app/doors/shared/order-dialog/order-dialog.component.ts
+++ src/app/doors/shared/order-dialog/order-dialog.component.ts
@@ -3,6 +3,8 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA, MatTableDataSource } from '@a
 import { FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
 import { DoorService } from '../../door.service';
+import {TranslateService} from '@ngx-translate/core';
+declare const $: any;
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -34,23 +36,39 @@ export class OrderDialogComponent implements OnInit {
   constructor(
     public dialogRef: MatDialogRef<OrderDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private doorService: DoorService
+    private doorService: DoorService,
+    private translate: TranslateService
   ) {}
 
   ngOnInit() {
     this.dialogRef.updateSize('350px');
   }
 
-  onNoClick(): void {
-    this.dialogRef.close();
-  }
   onSend(): void {
     this.phoneFormControl.markAsTouched();
     if (this.phoneFormControl.hasError('required')) return;
     this.dialogRef.close();
+    this.notify();
     this.doorService.order(this.nameFormControl.value, `+7${this.phoneFormControl.value}`, window.location.href)
       .subscribe(res => {
-
       });
+  }
+  notify(): void {
+    let from = 'top';
+    let align = 'right';
+    let type = 'info';
+    let message = this.translate.instant('doors.order-dialog.sent');
+
+    $.notify({
+      message: message,
+    }, {
+      placement: {from, align},
+      offset: {x: 20, y: 35},
+      type,
+      template: `<div class="alert alert-{0} alert-with-icon col-md-4">
+          <i class="material-icons alert-icon">notifications</i>
+          <span>{2}</span>
+        </div>`
+    });
   }
 }
