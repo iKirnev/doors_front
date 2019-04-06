d9646f4bbec4cb737a8e2f437a07dbee9974c881
Ivan <ivan.kirnev@gmail.com>
2019-04-06 11:56:42 +0300

    title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
+import { Title }     from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,12 @@ import {TranslateService} from '@ngx-translate/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private translate: TranslateService) {
+  constructor(private translate: TranslateService, private titleService: Title) {
     translate.setDefaultLang('ru');
+    let title = this.translate.instant('doors.order-dialog.sent');
+    this.setTitle( 'Металлические двери');
+  }
+  public setTitle( newTitle: string) {
+    this.titleService.setTitle( newTitle );
   }
 }
--- src/app/doors/calculator/calculator.component.ts
+++ src/app/doors/calculator/calculator.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CalculatorComponent } from '../shared/calculator/calculator.component';
+import {TranslateService} from '@ngx-translate/core';
+import { Title }     from '@angular/platform-browser';
 
 @Component({
   selector: 'app-calculator',
@@ -8,9 +10,14 @@ import { CalculatorComponent } from '../shared/calculator/calculator.component';
 })
 export class CalculatorPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private translate: TranslateService, private titleService: Title) { }
 
   ngOnInit() {
+    let title = this.translate.instant('nav_left.doors_calculator');
+    this.setTitle(title);
+  }
+  public setTitle( newTitle: string) {
+    this.titleService.setTitle( newTitle );
   }
 
 }
